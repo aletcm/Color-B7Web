185978cabdac648a55246dee0eda9b660dc0dc5a
Alessandro Taddei <alessandro.taddei@outlook.com>
2023-01-31 15:22:13 -0300

    First commit for this project (color manipulation)

---
+++ README.txt
@@ -0,0 +1,2 @@
+Este é um exercício de desafio proposto pela plataforma B7Web: Manipulação das cores através dos botões 
+em HTML-CSS e JavaScript.
---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+/* JS code requested by script.html file; Javascript content to HTML code */
+
+function blue() {
+    document.getElementById('core').style.backgroundColor = "#2222ff"
+}
+
+function red() {
+    document.getElementById('core').style.backgroundColor = "#ff0000"
+}
+
+function green() {
+    document.getElementById('core').style.backgroundColor = "#008000"
+}
+
+function yellow() {
+    document.getElementById('core').style.backgroundColor = "#ffd000"
+}
+
+function white() {
+    document.getElementById('core').style.backgroundColor = "#ffffff"
+}
